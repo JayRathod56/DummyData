e44dbcff0b5be8dcf874f5dfd479dc4314e3f997
jayrathod56 <jayantirathod56@gmail.com>
2025-01-18 00:36:32 +0530

    update 1 for cors headders

--- TestJson/settings.py
+++ TestJson/settings.py
@@ -48,9 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'TestJson.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
